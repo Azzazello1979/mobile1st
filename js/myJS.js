9f25404d47b214c5a68ea9f250749364f9c82496
Balint H <balint.haui@gmail.com>
2020-01-27 13:39:45 +0100

    CSS Grid with jQuery

---
+++ js/myJS.js
@@ -0,0 +1,23 @@
+let navEnabled = true;
+const gridWithNav = $('#wrapper').css('grid-template-areas');
+//console.log(gridWithNav);
+let currentGrid = gridWithNav;
+//console.log(currentGrid);
+
+$('#navButton').click(function(){
+    
+    if(navEnabled){
+        $('nav').remove();
+        let newGrid = currentGrid.replace('"nav nav nav nav nav nav nav nav"', '');
+        $('#wrapper').css('grid-template-areas', newGrid);
+        navEnabled = false;
+        console.log(navEnabled);
+    } else {
+        $( "<nav>Navigation</nav>" ).insertAfter( "header" );
+        $('#wrapper').css('grid-template-areas', gridWithNav);
+        navEnabled = true;
+        console.log(navEnabled);
+    }
+        
+
+})
